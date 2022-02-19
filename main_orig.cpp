b9316eedc6da0e060cbc1c717ffb79bdb9d3d2a0
jwgerlach00 <jwgerlach00@gmail.com>
2022-02-19 13:40:11 -0500

    Week 6 project temperature sensor w/ EEPROM completed

--- main_orig.cpp
+++ main_orig.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 #include <DHT.h>
-#include <EEPROM.h>
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
@@ -15,7 +14,11 @@ int thermistor_pin = A0;
 
 // Variables
 int thermistor_voltage;
-int stored_voltage;
+
+void setup() {
+  lcd.begin(16, 2);
+  dht.begin();
+}
 
 double voltage_to_temp(int v, bool f=false) {
   // Converts voltage from thermistor reading to temperature
@@ -34,26 +37,9 @@ double voltage_to_temp(int v, bool f=false) {
   }
 }
 
-void setup() {
-  lcd.begin(16, 2);
-  dht.begin();
-
-  // Display the last stored temp for 20 seconds
-  stored_voltage = EEPROM.read(0);
-  lcd.print("LAST STORED TEMP");
-  lcd.setCursor(0, 1);
-  lcd.print(String(voltage_to_temp(map(stored_voltage, 0, 255, 0, 1023))) + " Centigrade");
-  delay(20000);
-}
-
 void loop() {
   // Output thermistor data
   thermistor_voltage = analogRead(thermistor_pin);
-
-  // Save thermistor temp data to EEPROM
-  EEPROM.update(0, map(thermistor_voltage, 0, 1023, 0, 255));  // Originally write but update is more efficient
-
-  // Print thermistor to lcd
   lcd.setCursor(0, 0);
   lcd.print("Temp " + String(voltage_to_temp(thermistor_voltage)) + " C");
 
@@ -61,6 +47,6 @@ void loop() {
   lcd.setCursor(0, 1);
   lcd.print(String(dht.readTemperature()) + " C, " + String(dht.readHumidity()) + " %");
 
-  // Sample temperature about 1 time per minute
-  delay(60000);
+  // Update every 1/2 second
+  delay(500);
 }
